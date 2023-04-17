4e77595589fb76ceaff19720e50a156efe96e8fc
aksen123 <minwook0523@gmail.com>
2023-04-17 21:02:38 +0900

    230417

--- 4월/230412/script.js
+++ 4월/230412/script.js
@@ -1,11 +1,25 @@
 const night = document.querySelector(".night");
-const body = document.querySelector("body")
+
 
 night.onclick = () => {
-  body.classList.toggle("dayNight")
-  if(body.classList.contains("dayNight")) {
-    night.innerHTML = "day"
+  document.body.classList.toggle("dayNight")
+  if(night.innerHTML === "다크모드로 바꾸기") {
+    night.innerHTML = "라이트모드로 바꾸기"
   } else {
-    night.innerHTML = "night"
+    night.innerHTML = "다크모드로 바꾸기"
   }
 }
+
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+const modalBox = document.querySelector("#modal-box");
+
+
+open.addEventListener("click", () => {
+  modalBox.classList.toggle("active");
+  if(open.innerHTML === "프로필 보기"){
+    open.innerHTML = "프로필 닫기"
+  } else {
+    open.innerHTML = "프로필 보기"
+  }
+})
---
+++ 4월/portfolio/script.js
@@ -0,0 +1,57 @@
+const intBtn = document.querySelector("#intBtn");
+const pjBtn = document.querySelector("#pjBtn");
+const profileSection = document.querySelector(".profile .wrap");
+const projectSection = document.querySelector(".project .wrap");
+
+
+const left = document.querySelector(".left_scetion");
+const right = document.querySelector(".Right_page");
+
+// Section Btn
+
+intBtn.addEventListener("click", () => {
+  profileSection.classList.remove("on2")
+  projectSection.classList.add("on2")
+})
+pjBtn.addEventListener("click", () => {
+  projectSection.classList.remove("on2")
+  profileSection.classList.add("on2")
+})
+
+// Dark Btn
+const btns = document.querySelectorAll(".dark .bi");
+
+function night() {
+  left.style.color = "bisque"
+  left.style.backgroundColor = "#000"
+  // right.style.color = "bisque"
+  // right.style.backgroundColor = "#000"
+  profileSection.style.color = "bisque"
+  profileSection.style.backgroundColor = "#000"
+  projectSection.style.color = "bisque"
+  projectSection.style.backgroundColor = "#000"
+}
+function day() {
+  left.style.color = "#000"
+  left.style.backgroundColor = "#fff"
+  // right.style.color = "#000"
+  // right.style.backgroundColor = "#fff"
+  profileSection.style.color = "#000"
+  profileSection.style.backgroundColor = "#fff"
+  projectSection.style.color = "#000"
+  projectSection.style.backgroundColor = "#fff"
+}
+
+btns.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    if(e.target.id === 'day') {
+      btns[0].classList.toggle("active");
+      btns[1].classList.toggle("active2");
+      night();
+    } else if(e.target.id ==='night'){
+      btns[1].classList.toggle("active2");
+      btns[0].classList.toggle("active")
+      day();
+    }
+  })
+})
