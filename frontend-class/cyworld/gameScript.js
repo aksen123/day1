3c97a9b3e482cdddd3a7d8e4d051e33e3e00ac38
aksen123 <126546293+aksen123@users.noreply.github.com>
2023-05-14 15:39:51 +0900

    Update gameScript.js

--- frontend-class/cyworld/gameScript.js
+++ frontend-class/cyworld/gameScript.js
@@ -17,3 +17,27 @@ const startWord = () => {
 }
 
 
+//  lotto
+const lottoBtn = document.querySelector(".wrapper_lotto_btn");
+const gameNum = document.querySelector(".game_lotto_number");
+
+lottoBtn.addEventListener("click", ()=> {
+  gameNum.textContent ="";
+  let luckyArr = [...Array(45).keys()].map(el => el+1)
+  let result = []
+  for(let i = 0; i < 6; i++){
+    let idx = Math.floor(Math.random() * luckyArr.length);
+    let luckNum = luckyArr[idx];
+    result.push(luckNum)
+    // luckyArr[idx] = undefined;
+    luckyArr = luckyArr.filter(el => el !== luckyArr[idx]);
+    console.log(luckyArr)
+  }
+
+  result.sort((a, b) => a - b)
+  for(let el of result) {
+    const span = document.createElement('span');
+    span.innerText = el;
+    gameNum.appendChild(span);
+  }
+});
