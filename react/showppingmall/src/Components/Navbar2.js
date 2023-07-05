7c19d45a3fe3c4c4558b1fe0bda41945265be6ab
aksen123 <minwook0523@gmail.com>
2023-07-05 18:12:53 +0900

    .

--- react/showppingmall/src/Components/Navbar2.js
+++ react/showppingmall/src/Components/Navbar2.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
+
+
 const Navbar2 = () => {
+
   const menuList = [
     '여성',
     '남성',
@@ -11,8 +15,25 @@ const Navbar2 = () => {
     'Sale',
     '지속가능성'
   ]
+
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    navigate('/login')
+  }
+  const search = (event) => {
+    if(event.key ==='Enter') {
+      let keyword = event.target.value;
+      console.log(keyword)
+    }
+  }
   return (
     <div>
+      <div className='nav-header'>
+        <div onClick={()=> goToLogin()}>
+          <FontAwesomeIcon icon={faUser} />
+          <span style={{cursor: "pointer"}}>로그인</span>
+        </div>
+      </div>
       <div className='nav-logo'>
         <img width={100} src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/800px-H%26M-Logo.svg.png'/>
       </div>
@@ -25,10 +46,10 @@ const Navbar2 = () => {
             </a>
             </li>))}
         </ul>
-      </div>
       <div className='search-box'>
         <FontAwesomeIcon icon={faSearch}/>
-        <input type='text' placeholder='제품검색' />
+        <input type='text' placeholder='제품검색' onKeyPress={search}/>
+      </div>
       </div>
     </div>
   )
--- react/showppingmall/src/Components/ProductAll.js
+++ react/showppingmall/src/Components/ProductAll.js
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import { useEffect, useState } from 'react'
+import ProductCard from './ProductCard';
+import { Container, Row, Col } from 'react-bootstrap';
+// Container >> 수직 수평 정렬
+import 'bootstrap/dist/css/bootstrap.min.css'
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+
+  const getProduct = async() => {
+    let url = 'http://localhost:3004/products';
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data)
+    setProductList(data)
+    //url 에 담겨있던 json형식의 데이터를 productList에 담아줌
+  }
+  //화면이 렌더링 될때마다 화면에 가상 서버에서 데이터를 갖고온다
+  useEffect(()=>{
+    getProduct();
+  },[])
+
+
   return (
     <div>
-      전체상품 페이지
+      <Container>
+        <Row>
+          {productList.map((item) => (
+            <Col md={3} sm={12} key={item.id}>
+              <ProductCard item={item} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   )
 }
